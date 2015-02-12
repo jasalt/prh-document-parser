c6e87f168a62b042e916cc8033004138a779f240
Jarkko Saltiola <jarkko.saltiola@gmail.com>
2015-02-12 11:06:30 +0200

    Insert company data to db.

--- src/parser/db.py
+++ src/parser/db.py
@@ -5,7 +5,7 @@ import logging
 import psycopg2
 from unicodedata import normalize
 
-from sql import CREATE_ATTR_TABLE
+from sql import CREATE_ATTR_TABLE, CREATE_COMPANY_TABLE
 
 CONN_STR = "dbname='firm-db' user='dbuser' host='localhost' password='dbpass'"
 SCHEMAS = ["first_pass", "second_pass"]
@@ -29,7 +29,7 @@ def make_table_name(attr_u_str):
     normalized = normalize('NFKD', attr_u_str).encode('ascii', 'ignore')
     cleaned = reduce(lambda s, r: s.replace(r, "_"), [" ", "-", "__"],
                      normalized)
-    cleaned0 = cleaned if not cleaned[0] == "_" else cleaned[1:]
+    cleaned0 = cleaned if not cleaned[0] == "_" else cleaned[1:]  # TODO clean
     return cleaned0
 
 
@@ -41,33 +41,43 @@ def init_schema(cur, schema_name):
     except:
         print("Schema %s missing, unable to drop.") % schema_name
 
-    res = cur.execute("create schema %s" % schema_name)
+    do_sql(cur, "create schema %s" % schema_name)
     print("Created new schema %s." % schema_name)
-    return res
+
+    # Switch "scope" to selected schema
+    do_sql(cur, "set search_path to %s" % schema_name)
+    print("Set search path to schema %s." % schema_name)
+
+    do_sql(cur, CREATE_COMPANY_TABLE)
+    print("Initialized companies table")
 
 
-# TODO
-def insert_record(cur, schema, result):
+# TODO Validations
+def insert_record(cur, schema, parser_result):
     '''Insert parser result to database, table for each
         attribute'''
-    filename = result[0]
-    print "Inserting " + filename
-    # Switch "scope" to selected schema
-    do_sql(cur, "set search_path to %s" % schema)
+    filename = parser_result[0]
+    result = parser_result[1]
 
-    for record_entry in result[1]:
+    print "Inserting " + filename
+
+
+    firm_id = result['firmID']
+
+    do_sql(cur, '''INSERT INTO companies (name, firm_id, form)
+                   VALUES ('%s', '%s', '%s')''' %
+           (result['firmName'], firm_id, result['firmForm']))
+
+    for record_entry in result['log']:
         table_name = make_table_name(record_entry['title'])
         do_sql(cur, CREATE_ATTR_TABLE % table_name)
 
         content = normalize('NFKD',
                             record_entry['content']).encode('ascii', 'ignore')
 
-        # TODO Proper firm_id, validations, ...
-        res = do_sql(cur, '''INSERT INTO %s (firm_id, date, content)
-                             VALUES ('%s', '%s', '%s')''' %
-                     (table_name, 1, record_entry['date'], content))
-
-    return res
+        do_sql(cur, '''INSERT INTO %s (firm_id, date, content)
+                       VALUES ('%s', '%s', '%s')''' %
+               (table_name, firm_id, record_entry['date'], content))
 
 
 def connect():
